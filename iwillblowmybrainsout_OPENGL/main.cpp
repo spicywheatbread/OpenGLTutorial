d31d553ca4db071d982dee8c2ca7373048269ba1
Jonathan Luc <jonathan.luc@sjsu.edu>
2025-01-08 00:15:31 -0700

    Initial Commit

---
+++ iwillblowmybrainsout_OPENGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  iwillblowmybrainsout_OPENGL
+//
+//  Created by Jonathan Luc on 1/8/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
