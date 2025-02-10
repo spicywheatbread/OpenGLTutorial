9c0ac05a5f05370abb7ac7855d11cf4f833b98bb
Jonathan Luc <jonathanluc2010@yahoo.com>
2025-02-10 14:29:41 -0800

    Trying to set up cmake for part 4

--- 4_AdvancedOpenGL/src/main.cpp
+++ 4_AdvancedOpenGL/src/main.cpp
@@ -88,8 +88,8 @@ int main() {
     glViewport(0, 0, screenWidth, screenHeight);
     
     // Shader Compilation
-    Shader lightingShader("phongLighting.vert", "phongLighting.frag");
-    Shader outlineShader("phongLighting.vert", "shaderSingleColor.frag");
+    Shader lightingShader("resources/shaders/phongLighting.vert", "resources/shaders/phongLighting.frag");
+    Shader screenQuadShader("resources/shaders/screenQuad.vert", "resources/shaders/screenQuad.frag");
     
     /*
         Remember: to specify vertices in a counter-clockwise winding order you need to visualize the triangle
@@ -145,8 +145,6 @@ int main() {
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
         -0.5f,  0.5f,  0.5f,  0.0f, 0.0f  // bottom-left
     };
-
-
     
     float planeVertices[] = {
         // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
@@ -175,6 +173,17 @@ int main() {
         glm::vec3( 0.0f,  0.0f,  0.7f),
         glm::vec3(-0.3f,  0.0f, -2.3f),
         glm::vec3( 0.5f,  0.0f, -0.6f)
+    };
+    
+    float quadVertices[] = {
+        // positions   // texCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f, 1.0f
     };
     
     std::map<float, glm::vec3> sorted;
@@ -222,34 +231,75 @@ int main() {
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glBindVertexArray(0);
     
-    unsigned int cubeTexture  = loadTexture("marble.jpg");
-    unsigned int floorTexture = loadTexture("metal.png");
-    unsigned int grassTexture = loadTexture("grass.png");
-    unsigned int windowTexture = loadTexture("window.png");
+    unsigned int FBO;
+    glGenFramebuffers(1, &FBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, FBO);
     
+    unsigned int colorTexture;
+    glGenTextures(1, &colorTexture);
+    glBindTexture(GL_TEXTURE_2D, colorTexture);
+    // 1. Set texture dimensions to screen size
+    // 2. No data passed, simply allocating mem
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, screenWidth, screenHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorTexture, 0);
+    
+    unsigned int RBO; // Reminders:
+    glGenRenderbuffers(1, &RBO); // Generate ID
+    glBindRenderbuffer(GL_RENDERBUFFER, RBO); // Bind ID to target
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, screenWidth, screenHeight); // Allocate mem and specify internal format
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO); // Attach renderbuffer to framebuffer
+    
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    
+    // Screen VAO
+    unsigned int screenQuadVAO, screenQuadVBO;
+    glGenVertexArrays(1, &screenQuadVAO);
+    glGenBuffers(1, &screenQuadVBO);
+    glBindVertexArray(screenQuadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, screenQuadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GL_FLOAT), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GL_FLOAT), (void*)(2 * GL_FLOAT));
+    
+    unsigned int cubeTexture  = loadTexture("resources/textures/container.jpg");
+    unsigned int floorTexture = loadTexture("resources/textures/metal.png");
+    unsigned int grassTexture = loadTexture("resources/textures/grass.png");
+    unsigned int windowTexture = loadTexture("resources/textures/window.png");
+    
+    lightingShader.Activate();
     lightingShader.setInt("texture1", 0);
+    screenQuadShader.Activate();
+    screenQuadShader.setInt("screenTexture", 0);
     
     glEnable(GL_DEPTH_TEST);
     glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+    
     while(!glfwWindowShouldClose(window))
     {
+        glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
         // Calculate delta time so that device frame rate doesn't affect the controls
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
         
         processInput(window);
-        
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
-        // --------------------- Cameras --------------------- //
+
         glm::mat4 view = camera.GetViewMatrix();
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
         glm::mat4 model = glm::mat4(1.0f);
         
-        outlineShader.Activate();
-        outlineShader.setMat4("view", view);
-        outlineShader.setMat4("projection", projection);
+        
+        // glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // we're not using the stencil buffer now
+        glEnable(GL_DEPTH_TEST);
         
         lightingShader.Activate();
         lightingShader.setMat4("view", view);
@@ -273,32 +323,27 @@ int main() {
         lightingShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
                 
+        
         // CUBE 2 //
+        glBindTexture(GL_TEXTURE_2D, colorTexture);
         model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(2.0f, 1.0f, 0.0f));
         lightingShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
-        
-        // Vegetation
-        glBindVertexArray(grassVAO);
-        glBindTexture(GL_TEXTURE_2D, windowTexture);
 
         glDisable(GL_CULL_FACE);
-        for(std::map<float,glm::vec3>::reverse_iterator it = sorted.rbegin(); it != sorted.rend(); ++it) {
-            model = glm::mat4(1.0f);
-            model = glm::translate(model, it->second);
-            lightingShader.setMat4("model", model);
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-        }
-        /*
-        for(glm::vec3 pos : vegetation) {
-            model = glm::mat4(1.0f);
-            model = glm::translate(model, pos);
-            lightingShader.setMat4("model", model);
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-        }
-         */
         
+        /* second pass
+        glBindFramebuffer(GL_FRAMEBUFFER, 0); // back to default
+        glClear(GL_COLOR_BUFFER_BIT);
+          
+        glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+        screenQuadShader.Activate();
+        glBindVertexArray(screenQuadVAO);
+        glDisable(GL_DEPTH_TEST);
+        glBindTexture(GL_TEXTURE_2D, colorTexture);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        */ 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -307,6 +352,7 @@ int main() {
     glDeleteVertexArrays(1, &planeVAO);
     glDeleteBuffers(1, &cubeVBO);
     glDeleteBuffers(1, &planeVBO);
+    glDeleteFramebuffers(1, &FBO);
     
     lightingShader.Delete();
     glfwDestroyWindow(window);
